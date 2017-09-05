4573e3f3e7ae16d4abc394087bf6c389a317e1e0
James Clemens <jpclemens0@gmail.com>
2017-09-05 19:41:10 -0400

    Factored duplication of start_time and end_time.

--- babysitter_test.py
+++ babysitter_test.py
@@ -11,15 +11,17 @@ class TestBabysitter(unittest.TestCase):
         self.assertRaises(StartsTooEarly, self.babysitter.job_starts_at, start_time = datetime.time(16))
 
     def test_babysitter_sets_start_time_when_it_is_not_earlier_than_5PM(self):
-        self.babysitter.job_starts_at(datetime.time(17))
-        self.assertEqual(self.babysitter.start_time, datetime.time(17))
+        start_time = datetime.time(17)
+        self.babysitter.job_starts_at(start_time)
+        self.assertEqual(self.babysitter.start_time, start_time)
 
     def test_babysitter_ends_no_later_than_4AM(self):
         self.assertRaises(EndsTooLate, self.babysitter.job_ends_at, end_time = datetime.time(5))
 
     def test_babysitter_sets_end_time_when_it_is_not_later_than_4AM(self):
-        self.babysitter.job_ends_at(datetime.time(4))
-        self.assertEqual(self.babysitter.end_time, datetime.time(4))
+        end_time = datetime.time(4)
+        self.babysitter.job_ends_at(end_time)
+        self.assertEqual(self.babysitter.end_time, end_time)
 
 
 if __name__ == '__main__':
