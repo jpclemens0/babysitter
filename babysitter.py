f62e5abed9fe2af57e7ed372112b13392819e704
James Clemens <jpclemens0@gmail.com>
2017-09-05 19:19:40 -0400

    Added test for babysitter leaves no later than 4 AM.

--- babysitter.py
+++ babysitter.py
@@ -6,5 +6,12 @@ class Babysitter:
         if start_time < datetime.time(17):
             raise StartsTooEarly()
 
+    def job_ends_at(self, end_time):
+        if end_time > datetime.time(4):
+            raise EndsTooLate()
+
 class StartsTooEarly(Exception):
+    pass
+
+class EndsTooLate(Exception):
     pass
--- babysitter_test.py
+++ babysitter_test.py
@@ -8,6 +8,10 @@ class TestBabysitter(unittest.TestCase):
         babysitter = Babysitter()
         self.assertRaises(StartsTooEarly, babysitter.job_starts_at, start_time = datetime.time(16))
 
+    def test_babysitter_ends_no_later_than_4AM(self):
+        babysitter = Babysitter()
+        self.assertRaises(EndsTooLate, babysitter.job_ends_at, end_time = datetime.time(5))
+
 
 if __name__ == '__main__':
     unittest.main()
