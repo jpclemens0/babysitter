6ffbe219266644a35d1ba8c26eb1e642ec590fb5
James Clemens <jpclemens0@gmail.com>
2017-09-05 19:36:05 -0400

    Added a test to check that the start_time is set when job_starts_at is passed a valid start_time.

--- babysitter.py
+++ babysitter.py
@@ -5,6 +5,8 @@ class Babysitter:
     def job_starts_at(self, start_time):
         if start_time < datetime.time(17):
             raise StartsTooEarly()
+        else:
+            self.start_time = start_time
 
     def job_ends_at(self, end_time):
         if end_time > datetime.time(4):
--- babysitter_test.py
+++ babysitter_test.py
@@ -10,6 +10,10 @@ class TestBabysitter(unittest.TestCase):
     def test_babysitter_starts_no_earlier_than_5PM(self):
         self.assertRaises(StartsTooEarly, self.babysitter.job_starts_at, start_time = datetime.time(16))
 
+    def test_babysitter_sets_start_time_when_it_is_not_earlier_than_5PM(self):
+        self.babysitter.job_starts_at(datetime.time(17))
+        self.assertEqual(self.babysitter.start_time, datetime.time(17))
+
     def test_babysitter_ends_no_later_than_4AM(self):
         self.assertRaises(EndsTooLate, self.babysitter.job_ends_at, end_time = datetime.time(5))
 
