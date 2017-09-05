36448a8aa6771d0d3d2195df5b6142a1b3335a36
James Clemens <jpclemens0@gmail.com>
2017-09-05 19:26:20 -0400

    Factored out instantiation of babysitter into setUp.

--- babysitter_test.py
+++ babysitter_test.py
@@ -4,13 +4,14 @@ from babysitter import *
 
 class TestBabysitter(unittest.TestCase):
 
+    def setUp(self):
+        self.babysitter = Babysitter()
+
     def test_babysitter_starts_no_earlier_than_5PM(self):
-        babysitter = Babysitter()
-        self.assertRaises(StartsTooEarly, babysitter.job_starts_at, start_time = datetime.time(16))
+        self.assertRaises(StartsTooEarly, self.babysitter.job_starts_at, start_time = datetime.time(16))
 
     def test_babysitter_ends_no_later_than_4AM(self):
-        babysitter = Babysitter()
-        self.assertRaises(EndsTooLate, babysitter.job_ends_at, end_time = datetime.time(5))
+        self.assertRaises(EndsTooLate, self.babysitter.job_ends_at, end_time = datetime.time(5))
 
 
 if __name__ == '__main__':
