a971c7b823f0d96950147eee7dd2abd4d643b4fc
James Clemens <jpclemens0@gmail.com>
2017-09-11 09:09:55 -0400

    Updated formatting of the printed times and pay.

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ babysitter1 = Babysitter()
 babysitter1.job_starts_at(18, 30)
 babysitter1.bedtime_is(20, 15)
 babysitter1.job_ends_at(0, 20)
-print("The job begins at 6:30 PM, ends at 12:20 AM, and bedtime is 8:15 PM.")
-print("The pay for this job is " + str(babysitter1.calculate_pay()) + " dollars.")
+format = '%I:%M %p'
+print("The job begins at " + babysitter1.start_time.time.strftime(format) +
+      ", ends at " + babysitter1.end_time.time.strftime(format) +
+      ", and bedtime is " + babysitter1.bedtime.time.strftime(format) + ".")
+print("The pay for this job is $" + str(babysitter1.calculate_pay()) + ".")
